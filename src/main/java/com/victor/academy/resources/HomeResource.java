ed2349d10629cc05ac387aaa237df877a5c2f2a6
Victor Lourenço <victor.lourenco@tgid.com.br>
2022-10-23 17:43:40 -0300

    FEAT: Criando pagina HTML com Thymeleaf

---
+++ src/main/java/com/victor/academy/resources/HomeResource.java
@@ -0,0 +1,18 @@
+package com.victor.academy.resources;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeResource {
+	
+	@GetMapping("/")
+	public ModelAndView index() {
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("home/index");
+		mv.addObject("msg", "Mensagem do controller");
+		return mv;
+	}
+	
+}
